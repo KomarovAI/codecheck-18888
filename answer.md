da03b9dc2d8b34e94f2276f10384c83f6af2f5f0
code-project <dev@code-check.io>
2017-11-10 12:06:57 +0000

    Initial commit

---
+++ answer.md
@@ -0,0 +1,1 @@
+GG WP. play Dotka, drink Vodka!!!!11
---
+++ java/ai/src/main/java/codecheck/App.java
@@ -0,0 +1,13 @@
+package codecheck;
+
+public class App   {
+	public static void main(String[] args) {
+		for (int i = 0, l = args.length; i < l; i++) {
+            try {
+                			System.out.println("Ksd");
+            } catch (IOException e)  {
+                
+            }
+		}
+	}
+}
